ae4665b0d78b3b41910b803e175a69c8b15340f2
Vandana Ganwal <vandanaganwal12@gmail.com>
2024-02-27 13:03:57 +0530

    javascript

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
- const form = document.getElementById('form')
+const btn = document.getElementsByTagName('button')[0]
+// btn.addEventListener('onclick', (event) => {
+//     // event.preventDefault();
+//     const email = document.getElements('email');
+//     const mail = email.value
+//     console.log(mail)
+// });
 
-form.addEventListener('submit', (event) => {
-    // event.preventDefault();
-    const email = document.getElementById('email');
-    const mail = email.value
-    console.log(mail)
-});
+const urlParam = new URLSearchParams(window.location.search);
+const email = urlParam.get("email");
+
+document.getElementsByTagName("span")[0].innerText = email;
